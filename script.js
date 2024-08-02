1a334f42c02d40add3f68a78fc96dc955ed3d6da
hashaam44 <hashamsardar74@gmail.com>
2024-08-03 03:08:50 +0500

    Added

---
+++ script.js
@@ -0,0 +1,19 @@
+const menuToggle=document.querySelector('.menu-toggle');
+const menu=document.querySelector('.menu');
+const close=document.querySelector('.close');
+menuToggle.addEventListener('click',()=>{
+    menu.classList.toggle('active');
+})
+
+
+
+
+
+
+
+
+
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
