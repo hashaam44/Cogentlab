cb3560c28c28ce707e606bf3b53266d4f7062c8f
hashaam44 <hashamsardar74@gmail.com>
2024-08-03 10:02:41 +0500

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
+    var menuIcon = document.querySelector('.menu-icon');
     sidebar.classList.toggle('show');
+    menuIcon.classList.toggle('rotate');
 }
