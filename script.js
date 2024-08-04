3f7bb4c417580255ac56559183ad3fd869373abd
hashaam44 <hashamsardar74@gmail.com>
2024-08-04 14:57:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function toggleSidebar() {
-    var sidebar = document.getElementById('sidebar');
-    var menuIcon = document.querySelector('.menu-icon');
-    sidebar.classList.toggle('show');
-    menuIcon.classList.toggle('rotate');
+function toggleMenu() {
+    const button = document.getElementById('toggleButton');
+    const menu = document.getElementById('menu');
+    if (button.classList.contains('bar')) {
+        button.classList.remove('bar');
+        button.classList.add('arrow');
+        menu.classList.add('open');
+    } else {
+        button.classList.remove('arrow');
+        button.classList.add('bar');
+        menu.classList.remove('open');
+    }
 }
